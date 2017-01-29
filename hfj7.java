508d40dadbf63d0b838a0795f0cc395a2a419bf8
anshuman101 <anshuman.raina@gmail.com>
2017-01-29 14:53:30 +0530

    pattern errored

---
+++ hfj7.java
@@ -0,0 +1,27 @@
+public class Hfj7{
+	public static void main(String [] Args)
+	{
+		int Nrows=5;
+		int row=1;
+		while(row<=Nrows)
+		{
+			j=1;
+			while(j<=Nrows-row){
+				System.out.print(" ");
+				j=j++;
+			}
+		int value=row;
+		while(value<=2*row-1)
+			{
+				System.out.print(+value);
+				value++;
+			}
+		value=value-2;
+			while(value>=row)
+			{
+				System.out.print(+value);
+				value--;
+			}
+		}
+	}
+};
